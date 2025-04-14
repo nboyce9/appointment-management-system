add9b64a8cab735bb769ceecc6a932eaed04608e
nboyce <boycenobukho@gmail.com>
2025-04-14 04:55:24 +0200

    added Service classes

--- appointment-system/src/main/java/com/appointment/appointment_system/controller/UserController.java
+++ appointment-system/src/main/java/com/appointment/appointment_system/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.appointment.appointment_system.controller;
 
 import com.appointment.appointment_system.model.User;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 public class UserController {
 
     public List<User> getUsers(){
---
+++ appointment-system/src/main/java/com/appointment/appointment_system/service/AppointmentService.java
@@ -0,0 +1,7 @@
+package com.appointment.appointment_system.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppointmentService {
+}
