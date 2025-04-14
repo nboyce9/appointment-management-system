6fef4863c17e71a68987ee2c4df671504f719e67
nboyce <boycenobukho@gmail.com>
2025-04-14 04:48:57 +0200

    added a UserContrller file

---
+++ appointment-system/src/main/java/com/appointment/appointment_system/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.appointment.appointment_system.controller;
+
+import com.appointment.appointment_system.model.User;
+
+import java.util.List;
+
+public class UserController {
+
+    public List<User> getUsers(){
+
+    }
+}
