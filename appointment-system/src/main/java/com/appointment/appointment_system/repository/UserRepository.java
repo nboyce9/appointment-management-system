e29e71c1168843b826ba4975807c41ae0b59ba3f
nboyce <boycenobukho@gmail.com>
2025-04-14 07:18:51 +0200

    query annotation

--- appointment-system/src/main/java/com/appointment/appointment_system/repository/UserRepository.java
+++ appointment-system/src/main/java/com/appointment/appointment_system/repository/UserRepository.java
@@ -2,11 +2,13 @@ package com.appointment.appointment_system.repository;
 
 import com.appointment.appointment_system.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("SELECT u FROM User u Where u.emailAddress =?1")
     Optional<User> findByEmail(String email);
 }
