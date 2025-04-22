a7e19ec58af1d8394113211a9f90f19a15674a32
nboyce <boycenobukho@gmail.com>
2025-04-22 21:27:18 +0200

    added functionality for getting a list of appointments and an empty method for adding an appoinment

--- appointment-system/src/main/java/com/appointment/appointment_system/service/AppointmentService.java
+++ appointment-system/src/main/java/com/appointment/appointment_system/service/AppointmentService.java
@@ -1,7 +1,20 @@
 package com.appointment.appointment_system.service;
 
+import com.appointment.appointment_system.model.Appointment;
+import com.appointment.appointment_system.repository.AppointmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AppointmentService {
+    @Autowired
+    AppointmentRepository repo;
+    public List<Appointment> getAppointments(){
+        return repo.findAll();
+    }
+    public void addAppointment(){
+
+    }
 }
