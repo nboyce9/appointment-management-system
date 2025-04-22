a4f82f81b4797177ee2833c09d3000eaefba9e65
nboyce <boycenobukho@gmail.com>
2025-04-22 21:24:49 +0200

    modified the user class' variables by adding a list of appointments and removed the password, also used lombok :) how cool is lombok tho?git add src/main/java/com/appointment/appointment_system/controller/UserController.java

--- appointment-system/src/main/java/com/appointment/appointment_system/model/User.java
+++ appointment-system/src/main/java/com/appointment/appointment_system/model/User.java
@@ -3,6 +3,13 @@ package com.appointment.appointment_system.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
 
 @Entity
 @Table(name="usersdb")
@@ -13,76 +20,21 @@ public class User {
     private String surname;
     private String emailAddress;
     private String phoneNumber;
-    private String password;
     private UserRole role;
+    private List<Appointment> appointments;
 
     public User() {}
 
-    public User(Long id, String name, String surname,
-                String emailAddress, String phoneNumber,
-                String password, UserRole role) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.emailAddress = emailAddress;
-        this.phoneNumber = phoneNumber;
-        this.password = password;
-        this.role = role;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public UserRole getRole() {
-        return role;
-    }
-
-    public void setRole(UserRole role) {
-        this.role = role;
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", role=" + role +
+                ", appointments=" + appointments +
+                '}';
     }
 }
