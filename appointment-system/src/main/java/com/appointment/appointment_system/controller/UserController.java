9a254ff0859c727ef3e14bc83566c0cde8610c37
nboyce <boycenobukho@gmail.com>
2025-04-27 08:31:44 +0200

    added an endpoint for adding an appointment :)

--- appointment-system/src/main/java/com/appointment/appointment_system/controller/UserController.java
+++ appointment-system/src/main/java/com/appointment/appointment_system/controller/UserController.java
@@ -1,12 +1,10 @@
 package com.appointment.appointment_system.controller;
 
+import com.appointment.appointment_system.model.Appointment;
 import com.appointment.appointment_system.model.User;
 import com.appointment.appointment_system.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,10 @@ public class UserController {
     @PostMapping("/signup")
     public void addUser(@RequestBody User user){
         svc.addUser(user);
+    }
+
+    @PostMapping("/{user_id}/book")
+    public void addAppointment(@PathVariable int userId, @RequestBody Appointment appointment){
+        svc.addAppointment(userId, appointment);
     }
 }
