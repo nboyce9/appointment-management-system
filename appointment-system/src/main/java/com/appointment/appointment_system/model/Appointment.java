43311dc67f9f2b0c505ffbb9c07316980ccd2b75
nboyce <boycenobukho@gmail.com>
2025-04-27 08:27:25 +0200

    Json annotation to handle the serialization loop(on postman), and table relationship annotations plus user as foreign key

--- appointment-system/src/main/java/com/appointment/appointment_system/model/Appointment.java
+++ appointment-system/src/main/java/com/appointment/appointment_system/model/Appointment.java
@@ -1,19 +1,38 @@
 package com.appointment.appointment_system.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity
 public class Appointment {
+    @Id
+    @GeneratedValue
     private Integer id;
     private String location;
     private AppointmentStatus status;
     private LocalDate date;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    private User user;
 
     public Appointment(){}
 
-    public Appointment(String location, AppointmentStatus status, LocalDate date) {
+    public Appointment(Integer id, String location, LocalDate date, AppointmentStatus status, User user) {
+        this.id = id;
+        this.location = location;
+        this.date = date;
+        this.status = status;
+        this.user = user;
+    }
+
+    public Appointment(String location, AppointmentStatus status, LocalDate date, User user) {
         this.location = location;
         this.status = status;
         this.date = date;
+        this.user = user;
     }
 
     public String getLocation() {
@@ -40,6 +59,14 @@ public class Appointment {
         this.date = date;
     }
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     @Override
     public String toString() {
         return "Appointment{" +
@@ -47,6 +74,7 @@ public class Appointment {
                 ", location='" + location + '\'' +
                 ", status=" + status +
                 ", date=" + date +
+                ", user=" + user +
                 '}';
     }
 }
