085bf23412d5895dde0ba5736e1a5c5c1d4bd8a2
nboyce <boycenobukho@gmail.com>
2025-04-27 08:22:12 +0200

    added method for serving the adding and updating functionalities

--- appointment-system/src/main/java/com/appointment/appointment_system/service/UserService.java
+++ appointment-system/src/main/java/com/appointment/appointment_system/service/UserService.java
@@ -1,5 +1,6 @@
 package com.appointment.appointment_system.service;
 
+import com.appointment.appointment_system.model.Appointment;
 import com.appointment.appointment_system.model.User;
 import com.appointment.appointment_system.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,26 @@ public class UserService {
         if(user.getEmailAddress().isEmpty() || userOptional.isPresent()){
             throw new IllegalArgumentException("Email is already in use!");
         }
+        repo.save(user);
+    }
+
+    public void addAppointment(int userId, Appointment appointment) {
+        Optional<User> userOptional = repo.findById(userId);
+        if(userOptional.isEmpty()){
+            throw new IllegalArgumentException("Unknown user");
+        }
+        userOptional.get().addAppointment(appointment);
+        User user = userOptional.get();
+        repo.save(user);
+
+    }
+    public void updateAppointment(int userId, Appointment appointment){
+        Optional<User> userOptional = repo.findById(userId);
+        if(userOptional.isEmpty()){
+            throw new IllegalArgumentException("Unknown user");
+        }
+        userOptional.get().addAppointment(appointment);
+        User user = userOptional.get();
         repo.save(user);
     }
 }
